b128725b1b79e12f1a87c4bd00b72937116e3cb4
fiorenzo <fiorenzino@gmail.com>
2013-05-06 15:25:58 +0200

    some corrections

--- src/main/java/org/giavacms/jsfasciidoc/controller/AsciidocController.java
+++ src/main/java/org/giavacms/jsfasciidoc/controller/AsciidocController.java
@@ -1,17 +1,35 @@
 package org.giavacms.jsfasciidoc.controller;
 
 import java.io.Serializable;
+import java.util.Collections;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.internal.JRubyAsciidoctor;
 
 @Named
 @SessionScoped
 public class AsciidocController implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	public String getHtml() {
+	Asciidoctor asciidoctor;
 
-		return "";
+	public AsciidocController() {
+		System.out.println("creo AsciidocController");
+		asciidoctor = JRubyAsciidoctor.create();
+	}
+
+	public String getHtml() {
+		String rendered = asciidoctor.render("*This* is it.",
+				Collections.EMPTY_MAP);
+		System.out.println(rendered);
+		return rendered;
+	}
+
+	public static void main(String[] args) {
+		AsciidocController a = new AsciidocController();
+		a.getHtml();
 	}
 }
